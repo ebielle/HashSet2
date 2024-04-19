f26f30efedf5994cb7bff7f2e0e726da8354fd06
ebielle <eryblongo@gmail.com>
2024-04-19 12:24:43 +0200

    finished exercise

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println(riempiColori());
+    }
+
+    public static HashSet<String> riempiColori() {
+
+        HashSet<String> colori = new HashSet<>(Arrays.asList("Rosa", "Bianco", "Blu"));
+
+        HashSet<String> colori1 = new HashSet<>(Arrays.asList("Verde", "Giallo"));
+
+        colori.addAll(colori1);
+
+        System.out.println("Il colore verde fa parte del set di colori: " + colori.contains("Verde"));
+
+        return colori;
     }
 }
